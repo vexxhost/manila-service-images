7db4b74b626c6ab46941e946ba2cc1358e482904
Mohammed Naser <mnaser@vexxhost.com>
2026-05-19 15:24:51 -0400

    ci: run tempest from devstack venv
    Signed-off-by: Mohammed Naser <mnaser@vexxhost.com>

--- hack/smoke-test.sh
+++ hack/smoke-test.sh
@@ -3,8 +3,12 @@
 set -euo pipefail
 
 TEMPEST_DIR=${TEMPEST_DIR:-/opt/stack/tempest}
+TEMPEST_BIN=${TEMPEST_BIN:-/opt/stack/data/venv/bin/tempest}
+TEMPEST_CONFIG=${TEMPEST_CONFIG:-${TEMPEST_DIR}/etc/tempest.conf}
 TEMPEST_TEST_REGEX=${TEMPEST_TEST_REGEX:-manila_tempest_tests.tests.api.test_shares.SharesNFSTest.test_create_get_delete_share}
 
-cd "${TEMPEST_DIR}"
-tox -e all -- "${TEMPEST_TEST_REGEX}"
+export TEMPEST_CONFIG
 
+cd "${TEMPEST_DIR}"
+"${TEMPEST_BIN}" list-plugins
+"${TEMPEST_BIN}" run --regex "${TEMPEST_TEST_REGEX}"
